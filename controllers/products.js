9beb12cf254131cc6efb723fc024bb42baa36cd7
Cherry-Machado <Cherry5467@gmail.com>
2025-05-30 01:26:52 -0300

    Handling errors products.js controller.

--- controllers/products.js
+++ controllers/products.js
@@ -1,76 +1,93 @@
 const mongodb = require('../data/database');
-const ObjectId = require('mongodb').ObjectId; // This is the Id assigned automatically by MDB. 
+const { validateObjectId } = require('../utils/validate');
+const { NotFoundError } = require('../errors/databaseErrors');
 
-
-const getAll = async (req, res) => {
+const getAll = async (req, res, next) => {
     //#swagger.tags=["Products"];
-    const result = await mongodb.getDatabase().db().collection('products').find();
-    result.toArray().then((products) => {
+    try {
+        const result = await mongodb.getDatabase().db().collection('products').find();
+        const products = await result.toArray();
         res.setHeader('Content-Type','application/json');
         res.status(200).json(products);
-    });
+    } catch (error) {
+        next(error);
+    }
 };
 
-const getSingle = async (req, res) => {
+const getSingle = async (req, res, next) => {
     //#swagger.tags=["Products"];
-    const productId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().db().collection('products').find({ '_id': productId });
-    result.toArray().then((products) => {
+    try {
+        const productId = validateObjectId(req.params.id);
+        const product = await mongodb.getDatabase().db().collection('products').findOne({ '_id': productId });
+        if (!product) {
+            throw new NotFoundError('product');
+        }
         res.setHeader('Content-Type','application/json');
-        res.status(200).json(products[0]);
-    });
+        res.status(200).json(product);
+    } catch (error) {
+        next(error);
+    }
 };
 
-const createProduct = async (req, res) => {
+const createProduct = async (req, res, next) => {
     //#swagger.tags=["Products"];
-    const product = {
-        name: req.body.name,
-        brand: req.body.brand,
-        price: req.body.price,
-        stock: req.body.stock,
-        category: req.body.category,
-        isAvailable: req.body.isAvailable,
-        specifications: req.body.specifications
-    }
-    const response = await mongodb.getDatabase().db().collection('products').insertOne(product);
-    if (response.acknowledged > 0) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.error || 'Some error occurred while creating the product.');
+    try {
+        const product = {
+            name: req.body.name,
+            brand: req.body.brand,
+            price: req.body.price,
+            stock: req.body.stock,
+            category: req.body.category,
+            isAvailable: req.body.isAvailable,
+            specifications: req.body.specifications
+        };
+        const response = await mongodb.getDatabase().db().collection('products').insertOne(product);
+        if (response.acknowledged) {
+            res.status(201).json({ id: response.insertedId });
+        } else {
+            throw new Error('Error al crear el producto');
+        }
+    } catch (error) {
+        next(error);
     }
 };
 
-const updateProduct = async (req, res) => {
+const updateProduct = async (req, res, next) => {
     //#swagger.tags=["Products"];
-    const productId = new ObjectId(req.params.id);
-    const product = {
-        name: req.body.name,
-        brand: req.body.brand,
-        price: req.body.price,
-        stock: req.body.stock,
-        category: req.body.category,
-        isAvailable: req.body.isAvailable,
-        specifications: req.body.specifications
-    }
-    const response = await mongodb.getDatabase().db().collection('products').replaceOne({ '_id': productId }, product);
-    if (response.modifiedCount > 0) {
+    try {
+        const productId = validateObjectId(req.params.id);
+        const product = {
+            name: req.body.name,
+            brand: req.body.brand,
+            price: req.body.price,
+            stock: req.body.stock,
+            category: req.body.category,
+            isAvailable: req.body.isAvailable,
+            specifications: req.body.specifications
+        };
+        const response = await mongodb.getDatabase().db().collection('products').replaceOne({ '_id': productId }, product);
+        if (response.modifiedCount === 0) {
+            throw new NotFoundError('product');
+        }
         res.status(204).send();
-    } else {
-        res.status(500).json(response.error || 'Some error occurred while updating the product.');
+    } catch (error) {
+        next(error);
     }
 };
 
-const deleteProduct = async (req, res) => {
+const deleteProduct = async (req, res, next) => {
     //#swagger.tags=["Products"];
-    const productId = new ObjectId(req.params.id);
-    const response = await mongodb.getDatabase().db().collection('products').deleteOne({ '_id': productId });
-    if (response.deletedCount > 0) {
+    try {
+        const productId = validateObjectId(req.params.id);
+        const response = await mongodb.getDatabase().db().collection('products').deleteOne({ '_id': productId });
+        if (response.deletedCount === 0) {
+            throw new NotFoundError('product');
+        }
         res.status(204).send();
-    } else {
-        res.status(500).json(response.error || 'Some error occurred while deleting the product.');
+    } catch (error) {
+        next(error);
     }
 };
-
 
 module.exports = {
     getAll,
@@ -78,4 +95,4 @@ module.exports = {
     createProduct,
     updateProduct,
     deleteProduct
-}
+};
