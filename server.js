98b279c6e62b5b543e8c892d341a3d3542f39f0a
Cherry-Machado <Cherry5467@gmail.com>
2025-06-02 10:47:58 -0300

    Updating

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app
   saveUninitialized: false,
   cookie: { 
     secure: process.env.NODE_ENV === 'production',
-    httpOnly: false,
+    httpOnly: true,
     maxAge: 24 * 60 * 60 * 1000 // 1 day
   },
   store: MongoStore.create({
